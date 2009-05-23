801c8440be3674ba8197e238119335651ad7c5f5
mahout <mahout>
2009-05-23 18:04:31 +0000

    updated number.h with initial support for number.c's prime generator

---
+++ user/include/number.h
@@ -0,0 +1,26 @@
+/**
+ * @file	number.h
+ * @author	Sunil Beta Baskar <betasam@gmail.com>
+ * @brief	descriptors for numerical recipe data structures
+ */
+
+/**
+ * Copyright 2005-2009 (C) Sunil Beta Baskar
+ */
+
+
+#ifndef _NUMBER_H_INC
+#define _NUMBER_H_INC
+
+#include <btypes.h>
+
+#define		NUMBER_INT_SIZE		32
+
+#define		NUMBER_NIBBLE_FULL	0xF
+#define		NUMBER_NIBBLE_SIZE	4
+#define		NUMBER_NIBBLES_IN_INT	(NUMBER_INT_SIZE/NUMBER_NIBBLE_SIZE)
+
+#define		NUMBER_PRIME_ONE	3
+#define		NUMBER_PRIME_STEP	2
+
+#endif	/* _NUMBER_H_INC */
