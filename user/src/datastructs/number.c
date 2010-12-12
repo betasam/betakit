ebb8decfd47f25119aff9026320d730b926e727b
mahout <mahout>
2010-12-12 05:17:02 +0000

    updated with revised t_u64 implementations

--- user/src/datastructs/number.c
+++ user/src/datastructs/number.c
@@ -2,332 +2,284 @@
  * @file	number.c
  * @author	Sunil Beta Baskar <betasam@gmail.com>
  * @brief	integer arithmetic and floating point arithmetic structures
+ *
+ * @TODO	Add _CONFIG_RANDOM for dependency 
+ *		needed for random number generation 
  */
 
 
 /**
  * CHANGELOG
  *
+ * 2010-12-08   number_msbit( u64_number ) rewritten
+ *		number_lsbit( u64_number ) rewritten
+ *		number_mul( u32, u32 ) created
+ *
  * 2009-05-21	number_msbit( u_number ) implemented;
  *		number_gen_primes( ptr_u_primes, s_count ) defined;
  *		number_lsbit( u_number ) implemented;
  */
 
-/* bkit includes */
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* @remark bkit includes */
 #include <btypes.h>
 #include <memory.h>
 #include <ops.h>
 
-/* self definitions include <number.h> */
+/* @remark self definitions include <number.h> */
 #include <number.h>
 
-/* global data */
-
-t_u32 u_prime_table[15] = 
-  {  2,  3,   5,  7, 11,
-     13, 17, 19, 23, 29,
-     31, 37, 41, 43, 47 
-  };
+/* @remark global data */
 
 
 /**
- * @fn		number_msbit
- * @brief	finds the most significant bit in a number
+ * @fn number_msbit( t_u64 u64_number )
+ * @brief finds the MSB of a 64-bit unsigned integer
+ * @return returns the MSB (0...63)
  */
-t_s8 number_msbit( t_u32 u_number )
+t_u8 number_msbit( t_u64 u64_number )
 {
-  t_s8 s_retval = NUMBER_INT_SIZE;  
+  t_u8 retval = ZERO;
 
-  t_u8 u_nibble_count = 0;
-  t_u8 u_nibble = NUMBER_NIBBLE_FULL;
-  t_u8 u_bit_in_nibble = 0;
+  t_u32 u32_hold = ZERO;
+  t_u16 u16_hold = ZERO;
+  t_u8  u8_hold  = ZERO;
+  t_u8  u4_hold  = ZERO;
+  t_u8  u2_hold  = ZERO;
+  t_u8  bitshift = ZERO;
 
-  t_u32 u_complement = 0;
-
-  if( 0 == u_number )
+  /* @remark get rid of zero integers */
+  if( 0ull == u64_number )
     {
-      return( s_retval );
+      return(retval = bitshift = (SIZEOF_T_U64));
     }
 
-  /* 1's complement */
-  u_complement = ~(u_number); 
-
-  for(u_nibble_count = (NUMBER_NIBBLES_IN_INT-1); u_nibble_count >= 0; u_nibble_count--)
+  /* @remark find 32-bit part having MSB */
+  if( 0 != (u64_number & BK_NUMBER_UMASK))
     {
-      /* nibble count-down loop */
-      u_nibble = (NUMBER_NIBBLE_FULL << (u_nibble_count * NUMBER_NIBBLE_SIZE));
-
-      if( (u_complement & u_nibble) == 0 ) 
-	{
-	  /* nibble contains most significant bit */
-
-	  for( u_bit_in_nibble = (NUMBER_NIBBLE_SIZE-1); u_bit_in_nibble >= 0; u_bit_in_nibble-- )
-	    {	      
-	      /* bit count-down loop */
-	      if(u_number & ((1<<u_bit_in_nibble)<<(u_nibble_count*NUMBER_NIBBLE_SIZE)))
-		{
-		  s_retval = u_bit_in_nibble + (u_nibble_count * NUMBER_NIBBLE_SIZE);
-		  return( s_retval );
-		}
-	    }
-	}
-
+      u32_hold = (t_u32)((u64_number & BK_NUMBER_UMASK) >> SIZEOF_T_U32);
+      bitshift    += SIZEOF_T_U32;
     }
-   
-  return( s_retval );
+  else if( 0 != (u64_number & BK_NUMBER_LMASK))
+    {
+      u32_hold = (t_u32)((u64_number & BK_NUMBER_LMASK));
+    }
+ 
+  /* @remark find 16-bit part having MSB */
+  if( 0 != (u32_hold & BK_INTEGER_UMASK))
+    {
+      u16_hold = (t_u16)((u32_hold & BK_INTEGER_UMASK) >> SIZEOF_T_U16);
+      bitshift    += SIZEOF_T_U16;
+    }
+  else if( 0 != (u32_hold & BK_INTEGER_LMASK))
+    {
+      u16_hold = (t_u16)((u32_hold & BK_INTEGER_LMASK));      
+    }
+  
+  /* @remark find 8-bit part having MSB */
+  if( 0 != (u16_hold & BK_SHORT_UMASK))
+    {
+      u8_hold = (t_u8)((u16_hold & BK_SHORT_UMASK) >> SIZEOF_T_U8);
+      bitshift   += SIZEOF_T_U8;
+    }
+  else if( 0 != (u16_hold & BK_SHORT_LMASK))
+    {
+      u8_hold = (t_u8)((u16_hold & BK_SHORT_LMASK));
+    }
+
+  /* @remark find 4-bit nibble having MSB */
+  if(0 != (u8_hold & BK_BYTE_UMASK))
+    {
+      u4_hold = (t_u8)((u8_hold & BK_BYTE_UMASK) >> SIZEOF_T_U4);
+      bitshift   += SIZEOF_T_U4;
+    }
+  else if( 0 != (u8_hold & BK_BYTE_LMASK))
+    {
+      u4_hold = (t_u8)((u8_hold & BK_BYTE_LMASK));
+    }
+
+  /* @remark find 2-bit pair containing MSB */
+  if( 0 != (u4_hold & BK_NIBBLE_UMASK ))
+    {
+      u2_hold = (t_u8)((u4_hold & BK_NIBBLE_UMASK) >> SIZEOF_T_U2);
+      bitshift   += SIZEOF_T_U2;
+    }
+  else if( 0 != (u4_hold & BK_NIBBLE_LMASK))
+    {
+      u2_hold = (t_u8)((u4_hold & BK_NIBBLE_LMASK));
+    }
+
+  /* @remark find bit containing MSB */
+  if( _OPS_BIT(1) == (u2_hold & _OPS_BIT(1)))
+    {
+      bitshift += SIZEOF_T_BIT;
+    }
+    else if( _OPS_BIT(0) == (u2_hold & _OPS_BIT(0)) )
+    {
+      bitshift = bitshift;
+      /* @remark Do nothing */
+    }
+
+  retval = bitshift;
+  return( retval  ); /* @remark MSB: bit[63...0] numbering */ 
+}
+
+/**
+ * @fn number_lsbit( t_u64 u64_number )
+ * @brief finds the LSB of a 64-bit unsigned integer
+ * @return returns the MSB (0...63)
+ */
+t_u8 number_lsbit( t_u64 u64_number )
+{
+  t_u8 retval = ZERO;
+
+  t_u32 u32_hold = ZERO;
+  t_u16 u16_hold = ZERO;
+  t_u8  u8_hold  = ZERO;
+  t_u8  u4_hold  = ZERO;
+  t_u8  u2_hold  = ZERO;
+  t_u8  bitshift = ZERO;
+
+  /* @remark get rid of zero integers */
+  if( 0ull == u64_number )
+    {
+      return(retval = bitshift = (SIZEOF_T_U64 + 1));
+    }
+
+  /* @remark find 32-bit part having MSB */
+  if( 0 != (u64_number & BK_NUMBER_LMASK))
+    {
+      u32_hold = (t_u32)((u64_number & BK_NUMBER_LMASK));
+    }
+  else if( 0 != (u64_number & BK_NUMBER_UMASK))
+    {
+      u32_hold = (t_u32)((u64_number & BK_NUMBER_UMASK) >> SIZEOF_T_U32);
+      bitshift    += SIZEOF_T_U32;
+    }
+
+ 
+  /* @remark find 16-bit part having MSB */
+  if( 0 != (u32_hold & BK_INTEGER_LMASK))
+    {
+      u16_hold = (t_u16)((u32_hold & BK_INTEGER_LMASK));      
+    }
+  else if( 0 != (u32_hold & BK_INTEGER_UMASK))
+    {
+      u16_hold = (t_u16)((u32_hold & BK_INTEGER_UMASK) >> SIZEOF_T_U16);
+      bitshift    += SIZEOF_T_U16;
+    }
+
+  
+  /* @remark find 8-bit part having MSB */
+  if( 0 != (u16_hold & BK_SHORT_LMASK))
+    {
+      u8_hold = (t_u8)((u16_hold & BK_SHORT_LMASK));
+    }
+  else if( 0 != (u16_hold & BK_SHORT_UMASK))
+    {
+      u8_hold = (t_u8)((u16_hold & BK_SHORT_UMASK) >> SIZEOF_T_U8);
+      bitshift   += SIZEOF_T_U8;
+    }
+
+
+  /* @remark find 4-bit nibble having MSB */
+  if( 0 != (u8_hold & BK_BYTE_LMASK))
+    {
+      u4_hold = (t_u8)((u8_hold & BK_BYTE_LMASK));
+    }
+  else if(0 != (u8_hold & BK_BYTE_UMASK))
+    {
+      u4_hold = (t_u8)((u8_hold & BK_BYTE_UMASK) >> SIZEOF_T_U4);
+      bitshift   += SIZEOF_T_U4;
+    }
+
+
+  /* @remark find 2-bit pair containing MSB */
+  if( 0 != (u4_hold & BK_NIBBLE_LMASK))
+    {
+      u2_hold = (t_u8)((u4_hold & BK_NIBBLE_LMASK));
+    }
+  else if( 0 != (u4_hold & BK_NIBBLE_UMASK ))
+    {
+      u2_hold = (t_u8)((u4_hold & BK_NIBBLE_UMASK) >> SIZEOF_T_U2);
+      bitshift   += SIZEOF_T_U2;
+    }
+
+
+  /* @remark find bit containing MSB */
+  if( _OPS_BIT(0) == (u2_hold & _OPS_BIT(0)) )
+    {
+      bitshift = bitshift;
+      /* @remark Do nothing */
+    }
+  else if( _OPS_BIT(1) == (u2_hold & _OPS_BIT(1)))
+    {
+      bitshift += SIZEOF_T_BIT;
+    }
+
+  retval = bitshift;
+  return( retval  ); /* @remark LSB: bit[63...0] numbering */ 
 }
 
 
 /**
- * @fn		number_lsbit
- * @brief	finds the least significant bit in a number
+ * @fn t_u64 number_mul( t_u32 foo, t_u32 bar )
+ * @brief multiplies two 32-bit numbers to a 64-bit 
+ *	  operates on "J+" (or "Z+") only.
+ * @note This should be a macro
  */
-t_s8 number_lsbit( t_u32 u_number )
+inline t_u64 number_mul( t_u32 foo, t_u32 bar )
 {
-  t_s8 s_retval = NUMBER_INT_SIZE;  
-
-  t_u8 u_nibble_count = 0;
-  t_u8 u_nibble = NUMBER_NIBBLE_FULL;
-  t_u8 u_bit_in_nibble = 0;
-
-  t_u32 u_complement = 0;
-
-  if( 0 == u_number )
-    {
-      return( s_retval );
-    }
-
-  /* 1's complement */
-  u_complement = ~(u_number); 
-
-  for(u_nibble_count = 0; u_nibble_count < NUMBER_NIBBLES_IN_INT; u_nibble_count++)
-    {
-      /* nibble count-up loop */
-      u_nibble = (NUMBER_NIBBLE_FULL << (u_nibble_count * NUMBER_NIBBLE_SIZE));
-
-      if( (u_complement & u_nibble) == 0 ) 
-	{
-	  /* nibble contains least significant bit */
-
-	  for( u_bit_in_nibble = 0; u_bit_in_nibble < NUMBER_NIBBLE_SIZE; u_bit_in_nibble++)
-	    {	      
-	      /* bit count-up loop */
-	      if(u_number & ((1<<u_bit_in_nibble)<<(u_nibble_count*NUMBER_NIBBLE_SIZE)))
-		{
-		  s_retval = u_bit_in_nibble + (u_nibble_count * NUMBER_NIBBLE_SIZE);
-		  return( s_retval );
-		}
-	    }
-	}
-    }
-   
-  return( s_retval );
+  return( (t_u64)(foo * bar) );
 }
-
 
 /**
- * @fn		number_sqrt_z
- * @brief	returns the approximate integer square root of a number
- *		for positive integers only
+ * @fn t_u64 simple random number generator
+ * @brief generates unreliable psuedo random numbers
  */
-t_u32 number_sqrt_z( t_u32 u_number_zplus )
+t_u64 number_rand( t_u32 seed )
 {
-  t_s8 s_msb = 0;
+  static t_u64 prev_number;  
+  t_u64 ret_number;
+  t_u64 temp_num = 0ull;
+  time_t time_val;
+
+ nrlabel:
+
+  usleep(BK_SLEEP_USECS);
+ do {
+    time( &time_val );		/* @remark get time(...) @see <time.h> */
+    srand( (t_u32)time_val );	/* @remark set random seed */
+    ret_number = 0ull;
+ } while(0);
+
+  usleep(BK_SLEEP_USECS);
+  time_val = (time_t)rand();	/* @remark get random number (1) */
+
+  temp_num = (t_u32)time_val;
+  temp_num <<= SIZEOF_T_U32;
+
+  time( &time_val );
+  time_val = (time_t)rand();	/* @remark get random number (2) */
+
+#ifdef _NUMBER_DEBUG
+  printf("%s: 0x%08x 0x%08x\n", __FUNCTION__, time_val_1, time_val_2);
+#endif
+
+  ret_number = 0ull;
+  ret_number = (t_u64)( ((time_val & BK_NUMBER_LMASK) ) );
+  ret_number |= temp_num;
   
-  t_u32 u_retval = 0;
-  t_u32 u_root_lower = 0;
-  t_u32 u_root_upper = 0;
-  t_u32 u_curr_root = 0;
-  t_u32 u_prev_root = 0;
-
-  if( 0 == u_number_zplus) 
+  if( prev_number == ret_number )
     {
-      return( u_retval );
+      usleep(BK_SLEEP_USECS);
+      goto nrlabel;
     }
 
-  if( 1 == u_number_zplus)
-    {
-      u_retval = u_number_zplus;
-      return( u_retval );
-    }
-
-  s_msb = number_msbit( u_number_zplus );
-  /* DEBUG! here */
-
-  u_root_lower = (1<< ((s_msb + 1)/2 - 1));
-  u_root_upper = (1<< ((s_msb + 1)/2));
-
-  u_prev_root  = u_root_upper;
-  u_retval     = u_root_lower;
-
-  for( u_curr_root = u_root_upper; u_curr_root >= u_root_lower; u_curr_root-- )
-    {
-      /* count down to find approximate root */
-      if( (u_curr_root * u_curr_root) > u_number_zplus )
-	{
-	  u_retval = u_prev_root;
-	  return( u_retval );
-	}
-      else
-	{
-	  u_prev_root = u_curr_root;
-	}
-    }
-  
-  return( u_retval );
+  return( prev_number = ret_number );
 }
 
-
-/**
- * @fn		number_divisibility
- * @brief	tests whether an integer is divisible by a factor
- *		using just a modulo operator. It just has extra
- *		checks for 0 and 1 to avoid misinterpretations.
- * @comment	try to inline the function as the primality check is
- *		terrible in computation optimization
- */
-inline t_bool number_divisibility( t_u32 u_number_z, t_u32 u_factor_z )
-{
-  t_bool b_retval = false;
-
-  if( (0 == u_factor_z) || (1 == u_factor_z) )
-    {      
-      return( b_retval );
-    }
-
-  if( (1 == u_number_z) || (0 == u_number_z) )
-    {
-      return( b_retval );
-    }
-
-  if( 0 == (u_number_z % u_factor_z) )
-    {
-      b_retval = true;
-      return( b_retval );
-    }
-
-  return( b_retval );    
-}
-
-
-/**
- * @fn		number_test_prime
- * @brief	weak primality test, uses approximated root factors
- */
-t_bool number_test_prime( t_u32 u_number_zplus )
-{
-  t_bool b_retval = false;
-  
-  t_u32 u_number_zroot = 0;
-  t_u32 u_ctr = 0;
-
-  u_number_zroot = number_sqrt_z( u_number_zplus );
-  /* DEBUG! here */
-  
-  if( u_number_zroot > u_prime_table[14] )
-    {
-      for( u_ctr = 0; u_ctr < 14; u_ctr++ )
-	{
-	  if( true == number_divisibility(u_number_zplus, u_prime_table[ u_ctr ]) )
-	    {
-	      return(b_retval); /* b_retval is initialized to false */
-	    }
-	}
-    }
-  else
-    {
-      if( true == number_divisibility(u_number_zplus, NUMBER_PRIME_STEP))
-	{
-	  return(b_retval);
-	}
-
-      for( u_ctr = NUMBER_PRIME_ONE; u_ctr <= u_number_zroot; u_ctr += NUMBER_PRIME_STEP )
-	{
-	  if( true == number_divisibility(u_number_zplus, u_ctr) )
-	    {
-	      return(b_retval);
-	    }
-	}
-
-      /* CHECK! special case */
-      b_retval = true;
-      return( b_retval );
-    }
-  
-  
-  for( u_ctr = u_prime_table[14]; u_ctr <= u_number_zroot; u_ctr += NUMBER_PRIME_STEP )
-    {
-      /* test for primes larger than u_prime_table[14] */
-      if( true == number_divisibility(u_number_zplus, u_ctr) )
-	{
-	  return( b_retval );
-	}
-    }
-
-  b_retval = true;
-  return( b_retval );
-}
-
-
-/**
- * @fn		number_gen_primes
- * @brief	generates a series of prime numbers (32-bit only)
- */
-t_s32 number_gen_primes( t_u32 *ptr_u_primes, t_u32 u_count )
-{
-  t_s32 s_retval = -1;
-  t_u32 u_counter = 0;
-
-  t_u32 *ptr_u_new_primes;
-  t_u32 u_number_ref;
-
-  if( (u_count > 32) || (u_count <= 0) ) 
-    {
-      s_retval = -2;
-      return( s_retval );
-    }
-
-  if( 0 == ptr_u_primes )
-    {
-      s_retval = -3;
-      return( s_retval );
-    }
-
-  ptr_u_new_primes = ptr_u_primes;
-
-  *(ptr_u_new_primes) = 1;
-  *(ptr_u_new_primes + u_count - 1) = 1;
-  /* DEBUG! here. Fault on memory allocation : Dump Stova Core? */
-
-  if( u_count <= 15 )
-    {
-      for( u_counter = 0; u_counter < u_count; u_counter++ )
-	{
-	  *(ptr_u_new_primes + u_counter) = u_prime_table[ u_counter ];
-	}
-    }
-  else
-    {
-      for( u_counter = 0; u_counter < 15; u_counter++ )
-	{
-	  *(ptr_u_new_primes + u_counter) = u_prime_table[ u_counter ];	  
-	}
-
-      for( u_counter = 15; u_counter < u_count; u_counter++ )
-	{
-	  u_number_ref = *(ptr_u_new_primes + u_counter) = u_prime_table[ u_counter - 1 ] + 2;
-
-	  while( false == number_test_prime( u_number_ref ) )
-	    {
-	      u_number_ref = *(ptr_u_new_primes + u_counter) = (*(ptr_u_new_primes + u_counter) + 2);	      
-	      /* DEBUG! Danger we might just hang here. */
-	    }
-	  
-	}
-    }
-
-  s_retval = 0;			
-
-  return( s_retval );
-}
-
+/* @remark End of file "number.c" */
