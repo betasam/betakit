9851cc2e99b6785b51d0b81c6b7299edc2288de4
mahout <mahout>
2010-12-24 12:08:37 +0000

    trivial, initial version [empty]

---
+++ user/include/cli_parser.h
@@ -0,0 +1,11 @@
+/**
+ * @file	cli_parser.h
+ * @brief	support for cli_parser.c
+ * @see		cli_parser.c
+ * @date	2010-2012
+ */
+
+#ifndef _CLI_PARSER_H_INC
+#define _CLI_PARSER_H_INC
+
+#endif	/* _CLI_PARSER_H_INC */
