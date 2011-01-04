4de8a4bfff8085ed4676046a65e09820e3994aef
mahout <mahout>
2011-01-04 07:47:54 +0000

    updates on change to bkparse.pm, minor code changes tested check CHANGELOG

--- user/build/configure.pl
+++ user/build/configure.pl
@@ -13,19 +13,34 @@
 # exec: will be run in betakit/user/build 
 #	by GNU/Make
 # 
+# CHANGELOG
+#
+#	04-JAN-2011	bk_parse takes 4 arguments
+#			minor changes
+#
 use warnings;
 
 use bkparse;
 
-my $default_header_fnpath="../include/bkconfig.h";
+my $default_header_fnpath = "../include/bkconfig.h";
+my $default_oldconfig     = "../.bkconfig";
 
 my $argcount;
 
 sub print_config_usage()
 {    
     bk_print("Error! please specify root config file\n");   
+    bk_print("expected: uname\@host:~/betakit/user/build\$ ./configure ../root.bconfig\n");
+    bk_print("exiting!\n");
 }
 
+#
+# main()
+#	argv[1] = root.bconfig
+#	argv[2] = include/bkconfig.h
+#	argv[3] = .bkconfig
+#	argv[4] = (y/n) (force)
+#
 sub main()
 {
     $argcount = @ARGV;
@@ -38,17 +53,32 @@ sub main()
     exit 1;
     }
 
-    if( $argcount == 2 )
+    $header_fnpath = $default_header_fnpath;
+    $oldconfig_fnpath = $default_oldconfig;
+    $force = 'n';
+
+    if( $argcount >= 2 )
     {
 	$header_fnpath = $ARGV[1];
     }
-    else
+
+    if( $argcount >= 3 )
     {
-	$header_fnpath = $default_header_fnpath;
+	$oldconfig_fnpath = $ARGV[2];
+    }
+
+    if( $argcount >= 4 )
+    {
+	$force = $ARGV[3];
     }
 
     $filename = $ARGV[0];
-    bk_parse($filename, $header_fnpath);
+
+
+#    printf("file(%s), hdr(%s), old(%s), force(%s)\n",
+#	   $filename, $header_fnpath, $oldconfig_fnpath, $force);
+
+    bk_parse($filename, $header_fnpath, $oldconfig_fnpath, $force);
 
 }
 
