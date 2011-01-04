5293d1e6e4ecb287c07afe1c327fee0cf90b1815
mahout <mahout>
2011-01-04 07:50:48 +0000

    changes, allowing two additional command line arguments check CHANGELOG for additional information

--- user/build/pathfactor.pl
+++ user/build/pathfactor.pl
@@ -3,16 +3,23 @@
 #   allows you to replace paths specified as $BETAKITPATH$
 #   with the $TOP_DIR path
 #
+# CHANGELOG
+#
+#	04-JAN-2011	More configurability through 
+#			command line arguments added.
+#			still, only to be called by 
+#			TOP_DIR/Makefile
+#
 
 use strict;
 
 #
-# main( inputfile, path )
+# main( inputfile, path, leafdir, varname )
 # 
 #
 sub main
 {
-    my($filename,$path)=@_;
+    my($filename,$path,$leafdir,$varname)=@_;
     local *DXINFILE;
 
     if( $filename =~ /^$/ )
@@ -22,10 +29,20 @@ sub main
 	exit(2);	
     }
 
+    if( $leafdir =~ /^$/ )
+    {
+	$leafdir = "user";
+    }
+
+    if( $varname  =~ /^$/ )
+    {
+	$varname  = "BETAKITPATH";
+    }
+
     open(DXINFILE, "<" . $filename ) or die("trying to open '$filename' failed.");
 
     # running from $TOPDIR/user
-    $path =~ s/\/user(.*)$//;
+    $path =~ s/\/$leafdir(.*)$//;
 
     my $TOPDIR = $path;
 
@@ -35,7 +52,7 @@ sub main
 	my $txt_line = $_;
 	chomp( $txt_line );
 
-	$txt_line =~ s/\$BETAKITPATH\$/$TOPDIR/; 
+	$txt_line =~ s/\$$varname\$/$TOPDIR/; 
 
 	printf("%s\n", $txt_line );
     }
@@ -48,10 +65,10 @@ sub main
 
 if( (@ARGV[0] =~ /^$/) || (@ARGV[1] =~ /^$/) )
 {
-    printf("usage: $ ./build/pathfactor.pl <input file> <current dir>\n");
+    printf("usage: $ ./build/pathfactor.pl <input file> <current dir> [<leaf_dir> <var_name>]\n");
     printf("\t\texiting!\n");
     exit(3);	
 }
 
 
-main(@ARGV[0],@ARGV[1]);
+main(@ARGV[0],@ARGV[1],@ARGV[2]);
